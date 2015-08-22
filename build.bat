c6c221c61db8e79aa9adce654ba44c1654a32b6f
Roy Ivy III <RIVY.dev@gmail.com>
2015-08-22 12:56:13 -0500

    improve comments

--- build.bat
+++ build.bat
@@ -13,6 +13,8 @@ set src_dir=%__dp0%PCRE2-mirror
 :: user-configurable cmake project properties
 set "project_props="
 ::
+:: ref: http://pcre.org/current/doc/html/pcre2build.html @@ https://archive.is/rbI5U
+::
 ::set "project_props=%project_props% -D PCRE2_BUILD_PCRE2_8:BOOL=OFF" &:: build 8-bit PCRE library (default == ON; used by pcregrep)
 ::set "project_props=%project_props% -D PCRE2_BUILD_PCRE2_16:BOOL=ON" &:: build 16-bit PCRE library
 ::set "project_props=%project_props% -D PCRE2_BUILD_PCRE2_32:BOOL=ON" &:: build 32-bit PCRE library
@@ -20,10 +22,10 @@ set "project_props="
 ::set "project_props=%project_props% -D PCRE2_EBCDIC:BOOL=ON" &:: use EBCDIC coding instead of ASCII; (default == OFF)
 ::set "project_props=%project_props% -D PCRE2_EBCDIC_NL25:BOOL=ON" &:: use 0x25 as EBCDIC NL character instead of 0x15; implies EBCDIC; (default == OFF)
 ::
-::set "project_props=%project_props% -D PCRE2_LINK_SIZE:STRING=4" &:: internal link size (2 = 64k compiled pattern; 3 = ...)
+::set "project_props=%project_props% -D PCRE2_LINK_SIZE:STRING=4" &:: internal link size (in bytes) [ 2 (maximum 64Ki compiled pattern size ("gigantic patterns")), 3 ("truly enormous"), 4 ("truly enormous"+) ]
 ::set "project_props=%project_props% -D PCRE2_PARENS_NEST_LIMIT:STRING=500" &:: maximum depth of nesting parenthesis within regex pattern (default == 250)
-::set "project_props=%project_props% -D PCRE2GREP_BUFSIZE:STRING=51200" &:: internal buffer size (???) (default == 20480)
-set "project_props=%project_props% -D PCRE2_NEWLINE:STRING=ANYCRLF" &:: EOLN matching (default == LF)
+::set "project_props=%project_props% -D PCRE2GREP_BUFSIZE:STRING=51200" &:: internal buffer size (longest line length guaranteed to be processable) (default == 20480)
+set "project_props=%project_props% -D PCRE2_NEWLINE:STRING=ANYCRLF" &:: EOLN matching [CR, LF, CRLF, ANYCRLF, ANY (any Unicode newline sequence)] (default == LF) (NOTE: always overridable at run-time)
 ::set "project_props=%project_props% -D PCRE2_HEAP_MATCH_RECURSE:BOOL=ON" &:: OFF == use stack recursion; ON == use heap for recursion (slower); (default == OFF == stack recursion)
 ::set "project_props=%project_props% -D PCRE2_SUPPORT_JIT:BOOL=ON" &:: support for Just-In-Time compiling (default == OFF)
 ::set "project_props=%project_props% -D PCRE2_SUPPORT_PCRE2GREP_JIT:BOOL=OFF" &:: support for Just-In-Time compiling in pcre2grep (default == ON)
@@ -51,10 +53,10 @@ set "CMAKE_C_FLAGS="
 set "CMAKE_C_FLAGS=%CMAKE_C_FLAGS% -Wl,--stack,8388608" &:: 8Mi
 
 :: CMAKE_BUILD_TYPE
-set "CMAKE_BUILD_TYPE=-D CMAKE_BUILD_TYPE=MinSizeRel" &:: [<empty>, "-D CMAKE_BUILD_TYPE=Debug", "-D CMAKE_BUILD_TYPE=Release", "-D CMAKE_BUILD_TYPE=RelWithDebInfo", "-D CMAKE_BUILD_TYPE=MinSizeRel"]
+set "CMAKE_BUILD_TYPE=-D CMAKE_BUILD_TYPE=MinSizeRel" &:: [<empty/null>, "-D CMAKE_BUILD_TYPE=Debug", "-D CMAKE_BUILD_TYPE=Release", "-D CMAKE_BUILD_TYPE=RelWithDebInfo", "-D CMAKE_BUILD_TYPE=MinSizeRel"]
 
 :: using scoop (see "http://scoop.sh")
-:: `scoop install cmake gcc gow` &:: install 'cmake', 'gcc-tdw', and 'gow'
+:: `scoop install cmake gcc-tdw git gow` &:: install 'cmake', 'gcc-tdw' (multilib/32+64bit), and 'gow'
 
 :: create build directories
 mkdir "%build_dir%-x32"
